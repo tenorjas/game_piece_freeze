df6561cd76a403235ba172b80d21309ce06020fb
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-15 14:07:11 -0400

    declared classes, methods and variables

--- Program.cs
+++ Program.cs
@@ -4,9 +4,43 @@ namespace game_piece_freeze
 {
     class Program
     {
+        public class GamePiece
+        {
+            public int PositionX {get; set;}
+            public int PositionY {get; set;}
+            public bool Frozen {get; set;}
+            public string Name {get; set;}
+            public string Color {get; set;}
+
+            public void InitialConditions()
+            {
+                PositionX = 0;
+                PositionY = 0;
+                Frozen = false;
+            }
+
+            public void Move(int newX, int newY)
+            {
+                if (!Frozen)
+                {
+                    PositionX = newX;
+                    PositionY = newY;
+                }
+            }
+
+            public void Freeze()
+            {
+                Frozen = true;
+            }
+
+            public void Unfreeze()
+            {
+                Frozen = false;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
